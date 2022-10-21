19d84eda5fd611b300b4b8fac337aaea67cd415b
Anton Zarichny <antonzarichny@gmail.com>
2022-10-21 19:23:31 +0300

    Initial Commit

---
+++ ColorViewSUI/ColorViewSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewSUIApp.swift
+//  ColorViewSUI
+//
+//  Created by Антон Заричный on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
